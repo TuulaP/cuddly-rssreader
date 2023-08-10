7eadb868504467a9c8293148630143f64bd9666f
TuulaP <TuulaP@users.noreply.github.com>
2023-08-10 17:23:14 +0300

    Base idea

---
+++ rssreader.py
@@ -0,0 +1,104 @@
+
+import requests
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
+
+
+## trick to get away of one unneeded warning 
+from bs4.builder import XMLParsedAsHTMLWarning
+import warnings
+warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning)
+
+
+# base code from https://practicaldatascience.co.uk/data-science/how-to-read-an-rss-feed-in-python
+
+
+def get_source(url):
+    """Return the source code for the provided URL. 
+
+    Args: 
+        url (string): URL of the page to scrape.
+
+    Returns:
+        response (object): HTTP response object from requests_html. 
+    """
+
+    try:
+        session = HTMLSession()
+        response = session.get(url)
+
+        # print(response)
+
+        return response
+
+    except requests.exceptions.RequestException as e:
+        print(e)
+
+
+
+def get_feed(url):
+    """Return a Pandas dataframe containing the RSS feed contents.
+
+    Args: 
+        url (string): URL of the RSS feed to read.
+
+    Returns:
+        df (dataframe): Pandas dataframe containing the RSS feed contents.
+    """
+    
+    response = get_source(url)
+    
+    df = pd.DataFrame(columns = ['title', 'pubDate', 'guid', 'description'])
+   
+
+    with response as r:
+        items = r.html.find("item", first=False)
+
+        for item in items:        
+
+            title = item.find('title', first=True).text
+
+            #print("Title:", title)
+
+            pubDate = item.find('pubDate', first=True).text
+            guid = item.find('guid', first=True).text
+            description = item.find('description', first=True).text
+
+            link = item.find('link', first=True).text
+
+            row = {'title': title, 'pubDate': pubDate, 'guid': guid, 'description': description, 'link': link}
+            df = df.append(row, ignore_index=True)
+
+
+
+    return df
+
+
+
+url1 = "https://devclass.com/feed/"
+url2 = "https://practicaldatascience.co.uk/feed.xml"
+
+df1 = get_feed(url1) 
+df2 = get_feed(url2)
+
+
+result = pd.concat([df1, df2])
+
+shortinfo = result[["title", "guid", "pubDate"]]
+
+print(shortinfo)
+
+#for newsitem in shortinfo:
+#    print(newsitem['title']) # ['title'], newsitem['guid'])
+
+
+for index, row in shortinfo.iterrows():
+    # print(row)
+    title = row["title"]
+    row   = row["guid"]
+    # dte   = row["pubDate"]   ???
+
+    print('{}, {}'.format(title, row))
+
+
